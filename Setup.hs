35165e29a6feb3235c74dd1c21356030d2070e05
Christopher Reichert <creichert07@gmail.com>
2015-05-13 10:22:36 -0500

    Bump to 0.2.0.1.
    * Remove extras from Setup.hs.

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,2 @@
 import Distribution.Simple
-import Distribution.Simple.Program
-
-main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-       {
-         -- cabal-install is used during testing to run
-         -- and verify haddock documentation.
-         hookedPrograms = [ simpleProgram "cabal" ]
-       }
+main = defaultMain
